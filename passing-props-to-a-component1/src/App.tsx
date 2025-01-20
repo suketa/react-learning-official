15967b0306f4ae9b1b596045b22c4fd55d04b194
Masaki Suketa <m-suketa@ruby-dev.jp>
2025-01-20 19:01:39 +0900

    update

--- passing-props-to-a-component1/src/App.tsx
+++ passing-props-to-a-component1/src/App.tsx
@@ -2,9 +2,36 @@ import { getImageUrl } from "./utils.js";
 
 import Scientist from "./Scientist";
 
+type TPerson = {
+  name: string;
+  email: string;
+  address: string;
+}
+
+type FooProps = Pick<TPerson, "name" | "email">;
+
+function Foo({ name, email }: FooProps) {
+  return <div>{name} - {email}</div>;
+}
+
+const Person: Array<TPerson | string> = [
+  {
+    name: "Alice",
+    email: "alice@example.com",
+    address: "123 Main St"
+  }
+]
+
+function Bar() {
+  // return <Foo name={Person.name} email={Person.email} />;
+  return <Foo {...Person} />;
+  // return <Foo person={Person} />;
+}
+
 export default function Gallery() {
   return (
     <div>
+      <Bar />
       <h1>Notable Scientists</h1>
       <Scientist
         name="Maria Skłodowska-Curie"
